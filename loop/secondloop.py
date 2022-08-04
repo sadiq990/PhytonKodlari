a15ab2f3f3b90e400b2fdb5dfcf59ae9b9d269bc
unknown <musayevsadiq990@gmail.com>
2022-08-04 18:15:28 +0400

    Add existing project files to Git

---
+++ loop/secondloop.py
@@ -0,0 +1,14 @@
+# looplara ve iterasiyaya aid ders 2
+
+Usernames = ["admin1","admin2","admin3","admin4","admin5",]
+
+#iterasiya ya gore
+numbers = 0
+
+
+
+for users in Usernames:
+    numbers = numbers + 1 
+    print(numbers,users)
+
+    
