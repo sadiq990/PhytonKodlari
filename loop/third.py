ac83ef913bd7777018ec645443e4bea57b2205ef
unknown <musayevsadiq990@gmail.com>
2022-08-04 18:24:22 +0400

    split haqqinda

---
+++ loop/third.py
@@ -0,0 +1,8 @@
+# split massivdeki meselen ad soyadi iki yere ad ve soyada ayirir 
+# Meselen:
+
+employees = ["Sadig Musazade" , "Agsin Aligulov" , "Shamo Handamov"]
+
+name = employees[0].split()[0]
+surname = employees[0].split()[1]
+print (name, surname)
