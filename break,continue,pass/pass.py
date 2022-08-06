508397e5533ab51312c35a443e34d33252c59491
unknown <musayevsadiq990@gmail.com>
2022-08-06 20:34:45 +0400

    tek ve cut ededler

--- break,continue,pass/pass.py
+++ break,continue,pass/pass.py
@@ -9,3 +9,19 @@ for number in numbers:
         pass
     else:
         print(number)
+
+
+
+# amma bu tek cut kodu rahat olaraq belede yazmaq olardi
+numbers = range(1,10)     #   -------------> 1,2,3,4,5,6,7,8,9
+
+
+# tek ededler ucun 
+for number in numbers:
+    if number%2!=0:
+        print(number)
+
+#cut edeler ucun 
+for number in numbers:
+    if number%2!=1:
+        print(number)
