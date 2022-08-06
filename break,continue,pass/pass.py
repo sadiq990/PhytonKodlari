bf33412b499c740e050aff56ad253b74eeca40ef
unknown <musayevsadiq990@gmail.com>
2022-08-06 20:30:08 +0400

    pass haqqinda

---
+++ break,continue,pass/pass.py
@@ -0,0 +1,11 @@
+# about pass command
+
+numbers = range(1,10)     #   -------------> 1,2,3,4,5,6,7,8,9
+
+
+
+for number in numbers:
+    if number%2==0:
+        pass
+    else:
+        print(number)
