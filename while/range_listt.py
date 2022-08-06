9ea6502a8fe920b2c0661be2dbc3f8f016ddc02d
unknown <musayevsadiq990@gmail.com>
2022-08-06 18:55:30 +0400

    range haqqinda

---
+++ while/range_listt.py
@@ -0,0 +1,15 @@
+#range by default 0 dan ikinci verdiyimiz edede kimi ardicilqnan reqemleri verir :
+Sayilar = list(range(10))
+print(Sayilar)
+#biz burda birinci reqemi vermemisy deye, by default 0 dan 10 a kimi reqemleri cixarir
+
+
+#eger reqem veririkse onda:
+Sayilar = list(range(2,10))
+print(Sayilar)
+# o zaman , 2den 9 a kimi olan reqemler cixir
+
+# biz hettda 3 cu reqemide vere bilerik bu zaman
+
+Sayilar = list(range(2,10,2))
+print(Sayilar)
