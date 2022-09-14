ea87403a359cd65c5ec8dc803efbd3a382dee444
Sadig Musazade <SadigMusazade@risk.az>
2022-09-15 00:35:41 +0400

    lesson 15 range,zip,enumurate

---
+++ lesson_15_range,enumurate,zip/enumurate.py
@@ -0,0 +1,36 @@
+## enumurate is for ordering lists.
+#forexample:
+
+letter_list = ['a','b','c','d','e']
+
+for letter in letter_list:
+    print(letter) 
+# print result: a b c d e
+
+#now we are going to use enumurate for ordering.
+
+for  letter in enumerate(letter_list):
+    print(letter) 
+
+
+# print result
+# (0, 'a')
+# (1, 'b')
+# (2, 'c')
+# (3, 'd')
+# (4, 'e')
+
+
+# but with index we can use normal order 
+for  index,letter in enumerate(letter_list):
+    print(index,letter) 
+
+#print result: 
+# 0 a
+# 1 b
+# 2 c
+# 3 d
+# 4 e
+
+for  index,letter in enumerate(letter_list):
+    print(index+1,letter) 
